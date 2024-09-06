263d043d9401bf76ecd789f1cbcbd05633b70a02
Th14g0o <geekland135@gmail.com>
2024-09-06 16:39:14 -0300

    atulizando meu css

--- htmlEstatico/scripts/modal.js
+++ htmlEstatico/scripts/modal.js
@@ -1,5 +1,6 @@
 function AbrirFecharModal(id){
     let modal = document.getElementById(id);
-    if (modal.style.display != 'flex') modal.style.display = 'flex';
+    if (modal.style.opacity != 1) modal.style.opacity = 1;
+    else modal.style.opacity = 0;
     
 }
