c10694210b9e8c685ff2229d24295df304e65251
Th14g0o <geekland135@gmail.com>
2024-09-21 11:24:37 -0300

    footer e modelo com header e footer

--- htmlEstatico/scripts/modal.js
+++ htmlEstatico/scripts/modal.js
@@ -1,6 +1,16 @@
-function AbrirFecharModal(id){
+function AbrirFecharModal(id,  display = 'block'){
     let modal = document.getElementById(id);
-    if (modal.style.opacity != 1) modal.style.opacity = 1;
-    else modal.style.opacity = 0;
+    if (modal.style.opacity != 1){ 
+        modal.style.display = display;
+        setTimeout(function () {
+            modal.style.opacity = 1;
+        }, 20);
+    }
+    else {
+        modal.style.opacity = 0;
+        setTimeout(function () {
+            modal.style.display = "none"; 
+        }, 500);
+    }
     
 }
