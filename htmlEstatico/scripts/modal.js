0a6a43050bbfce66ba69a60a9d2188acdaa67cd2
Th14g0o <geekland135@gmail.com>
2024-09-28 19:06:42 -0300

    modal dretriz de criar post feito

--- htmlEstatico/scripts/modal.js
+++ htmlEstatico/scripts/modal.js
@@ -1,7 +1,8 @@
-function AbrirFecharModal(id,  display = 'block'){
+function AbrirFecharModal(id,  display = 'block', idArea = null){
     let modal = document.getElementById(id);
     if (modal.style.opacity != 1){ 
         modal.style.display = display;
+        if (idArea) document.getElementById(idArea).style.display = 'flex';
         setTimeout(function () {
             modal.style.opacity = 1;
         }, 20);
@@ -10,6 +11,7 @@ function AbrirFecharModal(id,  display = 'block'){
         modal.style.opacity = 0;
         setTimeout(function () {
             modal.style.display = "none"; 
+            if (idArea) document.getElementById(idArea).style.display = 'none';
         }, 500);
     }
     
